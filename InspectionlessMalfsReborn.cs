79a4f8b79f18ac0bbe2443c474abee1696f00220
inku <wcarpen4@gmail.com>
2026-09-03 17:17:40 -0400

    add bepinex config menu with debug

--- InspectionlessMalfsReborn.cs
+++ InspectionlessMalfsReborn.cs
@@ -1,4 +1,10 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using Comfort.Common;
+using EFT;
+using EFT.InventoryLogic;
+using UnityEngine;
 using InspectionlessMalfsReborn;
 
 namespace InspectionlessMalfs
@@ -6,10 +12,81 @@ namespace InspectionlessMalfs
     [BepInPlugin("com.inspectionlessmalfsreborn.inku", "InspectionlessMalfsReborn", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public const string PLUGIN_GUID = "com.inku.inspectionlessmalfsreborn";
+        public const string PLUGIN_NAME = "InspectionlessMalfsReborn";
+        public const string PLUGIN_VERSION = "1.0.0";
+        public static ConfigEntry<bool> ModEnabled { get; private set; }
+        public static ConfigEntry<bool> DebugEnabled { get; private set; }
+        public static ConfigEntry<KeyboardShortcut> ForceMalfKey { get; private set; }
+        public static ManualLogSource Log { get; private set; }
+        
         private void Awake()
         {
+            Log = Logger;
+            
+            // BepInEx Menu Configuration Entries
+            ModEnabled = Config.Bind(
+                "1. General",
+                "Mod Enabled",
+                true,
+                "Enable or disable inspectionless malfunction identification. ON - clear malfunctions without inspecting. OFF - vanilla EFT behavior."
+            );
+
+            DebugEnabled = Config.Bind(
+                "2. Debug",
+                "Debug Enabled",
+                false,
+                "Allow debug options to take effect, like forced weapon malfunctions."
+            );
+
+            ForceMalfKey = Config.Bind(
+                "2. Debug",
+                "Force Malfunction Key",
+                new KeyboardShortcut(KeyCode.End),
+                "Keybind to force a malfunction on currently equipped weapon."
+            );
+            
             new KnowMalf().Enable();
-            Logger.LogInfo("InspectionlessMalfsReborn is loaded!");
+            Log.LogInfo($"{PLUGIN_NAME} is loaded!");
+        }
+        
+        private void Update()
+        {
+            if (DebugEnabled.Value && ForceMalfKey.Value.IsDown())
+            {
+                ForceWeaponMalfunction();
+            }
+        }
+
+        private void ForceWeaponMalfunction()
+        {
+            var gameWorld = Singleton<GameWorld>.Instance;
+            if (gameWorld == null || gameWorld.MainPlayer == null)
+            {
+                return;
+            }
+            
+            Weapon.EMalfunctionState[] malfunctionTypes = new[]
+            {
+                Weapon.EMalfunctionState.Misfire,
+                Weapon.EMalfunctionState.Jam,
+                Weapon.EMalfunctionState.Feed,
+                Weapon.EMalfunctionState.HardSlide,
+                Weapon.EMalfunctionState.SoftSlide
+            };
+
+            Player player = gameWorld.MainPlayer;
+            if (player.HandsController is Player.FirearmController firearmController)
+            {
+                Weapon weapon = firearmController.Item;
+                if (weapon == null) return;
+
+                // force a random malfunction state on the current firearm
+                Weapon.EMalfunctionState randomMalf = malfunctionTypes[UnityEngine.Random.Range(0, malfunctionTypes.Length)];
+                weapon.MalfState.State = randomMalf;
+                
+                Log.LogInfo($"[{PLUGIN_NAME}] Successfully forced random malfunction - {randomMalf} - on active weapon ({weapon.ShortName.Localized()}).");
+            }
         }
     }
 }
--- Patch.cs
+++ Patch.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using SPT.Reflection.Patching;
 using HarmonyLib;
 using EFT.InventoryLogic;
+using InspectionlessMalfs;
 
 namespace InspectionlessMalfsReborn
 {
@@ -10,13 +11,21 @@ namespace InspectionlessMalfsReborn
     {
         protected override MethodBase GetTargetMethod()
         {
-            return AccessTools.FirstMethod(typeof(Weapon.MalfunctionState), method => method.Name == nameof(Weapon.MalfunctionState.IsKnownMalfType)
+            return AccessTools.FirstMethod(
+                typeof(Weapon.MalfunctionState), 
+                method => method.Name == nameof(Weapon.MalfunctionState.IsKnownMalfType)
             );
         }
 
         [PatchPostfix]
         private static void PatchPostfix(ref bool __result)
         {
+            // If the toggle in BepInEx menu is disabled, skip modifying the result
+            if (!Plugin.ModEnabled.Value)
+            {
+                return;
+            }
+
             __result = true;
         }
     }
